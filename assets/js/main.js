b49fe60f42ff49310bb767b0cbe38d86d5d83b90
Daniil-Belik <daniilbelikk@gmail.com>
2023-03-14 13:20:10 +0200

    готово

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,11 @@ $(document).ready(function () {
   });
 
 });
+$(document).ready(() => {
+
+  $('.header-navbar__btn').on('click', function () {
+    $(this).toggleClass('active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500)
+  });
+
+});
